104d09ef73e0e56b164b8edfd35ee9a8f9dd12ff
Irena <irenejpopova6@gmail.com>
2020-06-05 11:08:54 +0200

    routers

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const storeController = require('../controllers/storeController');
 
 // Do some more work here
-router.get('/', (req, res) => {
+/* router.get('/', (req, res) => {
   const irena = {
     name: 'irena',
     age: '35',
@@ -11,11 +12,17 @@ router.get('/', (req, res) => {
   // console.log("Hey!!!")
   // res.send("Hey! It works");
   res.json(req, query);
-});
+}); */
 
-router.get('/reverse/:name', (req, res) => {
+/* router.get('/reverse/:name', (req, res) => {
   const reverse = [...req.params.name].reverse().join('');
   res.send(reverse);
-})
+}) */
 
-module.export = router;
+router.get('/', storeController.middleWare, storeController.homePage);
+
+module.export = router; // my routes files exports my router which will handle the individual forward slash routes that are needed, 
+
+// all is sequencial 
+// Modals are where the Data is going to be stored , and before I can  actually create a piece of data, I am going to be able to create stores. Before i CAN CREATE A PIECE OF DATA i need to  describe what that data will look like. The simplest way I can put it in ahead of time.
+// i am doing everything in strict mode so I have to define my schema beforehand. 

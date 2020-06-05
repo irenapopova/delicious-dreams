bdd2d85df32c9cefc8e2c18740a9b0c3112b63cc
Irena <irenejpopova6@gmail.com>
2020-06-05 11:08:09 +0200

    storeController

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,9 +1,14 @@
+// req -is the data that comes in (user signs in with <form>  and POSTs to /login)
+// res is the data that comes out and in between the req & res a bunch of work must happen
 export.myMiddleware = (req, res, next) => {
   req.name = 'irena';
-  next();
+  
+  next(); // 
 };
 
 exports.homePage = (req, res) => {
   console.log(req.name);
   res.render('index');
 };
+
+//middleWare allows to run code after the request but before response 
